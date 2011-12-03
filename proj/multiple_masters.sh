e95fbb8c033f9a61cb9c43be4cbe155938f43994
Erik Reed <ereed.uw@gmail.com>
2011-12-03 00:20:37 +0000

    multiple master

---
+++ proj/multiple_masters.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#./master.sh net_to_fg/AdaptSubsets/adapt_mini2.net
+#./master.sh net_to_fg/AdaptSubsets/adapt_mini3.net
+./master.sh net_to_fg/AdaptSubsets/adapt_subset1.net
+./master.sh net_to_fg/AdaptSubsets/adapt_subset2_large.net
+
