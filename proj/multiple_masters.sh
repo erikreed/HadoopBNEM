329f10fd56a3bea68de363e0acb07d7da3c67551
Erik Reed <ereed.uw@gmail.com>
2012-02-12 08:52:10 +0000

    tweaks for shared parameter order/sorting

--- proj/multiple_masters.sh
+++ proj/multiple_masters.sh
@@ -4,10 +4,10 @@
 # newerouput (i.e. 2 hidden) 15 | testbed_setup
 
 DEST=new_bnet_results
-
+MIN_SHARED=10
 #BNETS="adapt_mini1 adapt_subset2_large adapt10_v7e \
 	#		diabetes link mildew pigs water"
-BNETS="alarm water pigs"  #pigs water link adapt10_v7e mildew diabetes"
+BNETS="pigs ADAPT_PHM10_P2 link"  #pigs water link adapt10_v7e mildew diabetes"
 BNET_DIR=bnets
 
 export NUM_SAMPLES=5000
@@ -23,9 +23,11 @@ for net in $BNETS
 do
 	# note this only returns shareds >= 2 
 	shared=`python net_to_fg/read_net_erik.py $BNET_DIR/$net.net`
-	echo set of possible shared params for $net: $shared
-	for s in $shared
+	shared_sorted=`echo $shared | tr " " "\n" | sort -g -r`
+	echo set of possible shared params for $net: $shared_sorted
+	for s in $shared_sorted
 	do
+		if (( MIN_SHARED <= s )); then
 		sharedvars=`python net_to_fg/read_net_erik.py $BNET_DIR/$net.net $s`
 		echo trying hidden/shared nodes: $sharedvars
 
@@ -41,6 +43,7 @@ do
 			./master.sh $BNET_DIR/$net.net
 			mv out $DEST/$net/h$s
 		fi
+	fi
 	done
 	echo completed $net
 done
