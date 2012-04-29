06a6aadc355863ff9779137e82218309cfe9ed15
Erik Reed <ereed.uw@gmail.com>
2012-04-29 10:39:00 +0000

    alem almost done. infinite loop.

--- mapreduce/src/alem.cpp
+++ mapreduce/src/alem.cpp
@@ -2,74 +2,139 @@
 // erikreed@cmu.edu
 
 #include "dai_mapreduce.h"
+#include <boost/foreach.hpp>
+
+//#include <boost/shared_ptr.hpp>
+//typedef boost::shared_ptr<InfAlg> InfAlgPtr;
 
 const size_t pop_size = 10; // i.e. EMruns, denoted N
 const size_t numLayers = 6;
 const double agegap = 5; // denoted a
 
+FactorGraph fg; // todo: refactor in
+PropertySet infprops;
+
+EMAlg *initEMAlg(FactorGraph fg, PropertySet infprops) {
+    Evidence* e = new Evidence();
+    randomize_fg(&fg);
+//    InfAlgPtr inf = InfAlgPtr(newInfAlg(INF_TYPE, fg, infprops));
+//    inf.get()->init();
+    InfAlg* inf = newInfAlg(INF_TYPE, *fg.clone(), infprops);
+
+    ifstream estream("dat/tab");
+    e->addEvidenceTabFile(estream, *fg.clone());
+    // Read EM specification
+    ifstream emstream("dat/em");
+    EMAlg *newEMalg = new EMAlg(*e, *inf, emstream);
+    return newEMalg;
+}
+
+void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t*  min_runs, size_t layer, size_t index) {
+	assert(layer+1 < emAlgs.size());
+
+	EMAlg* em = emAlgs[layer][index];
+	Real likelihood = em->logZ();
+
+	if (emAlgs[layer+1].size() < min_runs[layer+1]) {
+		emAlgs[layer+1].push_back(em);
+	}
+	else {
+		// find out if there is a worse run in next layer
+		for (size_t j=0; j<emAlgs[layer+1].size(); j++) {
+			Real next = emAlgs[layer+1][j]->logZ();
+			if (next < likelihood) {
+				// discard em_next
+				EMAlg* discarded = emAlgs[layer+1][j];
+				emAlgs[layer+1].erase(emAlgs[layer+1].begin() + j);
+				delete discarded;
+				cout << "Deleted run from layer " << layer << endl;
+				// insert em
+				emAlgs[layer+1].push_back(em);
+				break;
+			}
+		}
+	}
+
+	// remove em from original layer
+	emAlgs[layer].erase(emAlgs[layer].begin()+index);
+}
+
+void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t* ageLimit) {
+
+	size_t runsTerminated = 0;
+	// while non-terminated runs exist
+	while (runsTerminated < pop_size) {
+		for (size_t i=0; i<numLayers; i++) {
+
+			// if first layer...
+			if (i == 0 &&  emAlgs[i].size() < min_runs[0]) {
+				// insert k new EM runs
+				int k = min_runs[i] - emAlgs[i].size();
+				for (int j=0; j<k; j++)
+					emAlgs[i].push_back(initEMAlg(fg,infprops));
+				// todo: verify that all of layer 0 is iterated
+				BOOST_FOREACH(EMAlg* em, emAlgs[i]) {
+					em->iterate();
+				}
+			}
+			for (size_t j=0; j<emAlgs[i].size(); j++){
+				EMAlg* em = emAlgs[i][j];
+				// em trial has terminated
+				if (em->hasSatisfiedTermConditions())
+					runsTerminated++;
+				else if (em->Iterations() == ageLimit[i])
+					checkRuns(emAlgs,min_runs, i, j);
+			}
+		}
+	}
+}
+
+
+
 int main(int argc, char* argv[]) {
 
-	FactorGraph fg;
-	fg.ReadFromFile("dat/fg");
 
-	// Prepare junction-tree object for doing exact inference for E-step
-	PropertySet infprops;
+	fg.ReadFromFile("dat/fg");
 	infprops.set("verbose", (size_t) 0);
 	infprops.set("updates", string("HUGIN"));
 	infprops.set("log_z_tol", LIB_EM_TOLERANCE);
 	infprops.set("MAX_ITERS", EM_MAX_ITER);
 
-
-	vector<InfAlg*> algs;
-	vector<vector <EMAlg*> > emAlgs;
+	vector<vector<EMAlg*> > emAlgs;
 	for (size_t i = 1; i < numLayers; i++)
-		emAlgs.push_back(vector<EMAlg*>());
+		emAlgs.push_back(vector<EMAlg*> ());
 
 	// beta_i (age limit of layer i)
-	int* ageLimit = new int[numLayers];
+	size_t* ageLimit = new size_t[numLayers];
 	for (size_t i = 0; i < numLayers - 1; i++)
-		ageLimit[i] = agegap * pow(2,i-1);
-	ageLimit[numLayers-1] = EM_MAX_ITER;
+		ageLimit[i] = agegap * pow(2, i - 1);
+	ageLimit[numLayers - 1] = EM_MAX_ITER;
 
 	// M_i (min number of runs of layer i)
-	int* min_runs = new int[numLayers];
+	size_t* min_runs = new size_t[numLayers];
 	min_runs[0] = 5;
 	for (size_t i = 1; i < numLayers - 1; i++)
 		min_runs[i] = 2;
-	min_runs[numLayers-1] = EM_MAX_ITER;
+	min_runs[numLayers - 1] = EM_MAX_ITER;
 
 
-	// initialize InfAlgs
+	// initialize first layer
 	for (size_t i = 0; i < pop_size; i++) {
-		randomize_fg(&fg);
-		InfAlg* inf = newInfAlg(INF_TYPE, fg, infprops);
-		inf->init();
-		algs.push_back(inf);
-	}
 
-	// initialize EMAlgs
-	for (size_t i = 0; i < pop_size; i++) {
-		Evidence e;
-		ifstream estream("dat/tab");
-		e.addEvidenceTabFile(estream, fg);
-
-		// Read EM specification
-		ifstream emstream("dat/em");
+		EMAlg *newEMalg = initEMAlg(fg, infprops);
+		newEMalg->iterate();
 
 		// add EMAlg to first layer
-		emAlgs[0].push_back(new EMAlg(e, *algs[i], emstream));
-
+		emAlgs[0].push_back(newEMalg);
 	}
 
-
-	// perform initial EM run:
-	for (size_t i = 0; i < pop_size; i++)
-		emAlgs[0][i]->iterate();
+	// start ALEM
+	ALEM_check(emAlgs, min_runs, ageLimit);
 
 	// collect best likelihood
 	Real bestLikelihood = -1e100;
 	for (size_t i = 0; i < numLayers; i++) {
-		for (size_t j=0; j<emAlgs[i].size(); j++) {
+		for (size_t j = 0; j < emAlgs[i].size(); j++) {
 			bestLikelihood = max(bestLikelihood, emAlgs[i][j]->logZ());
 			delete emAlgs[i][j];
 		}
@@ -77,8 +142,6 @@ int main(int argc, char* argv[]) {
 
 	cout << "Best: " << bestLikelihood << endl;
 
-	for (size_t i = 0; i < pop_size; i++)
-		delete algs[i];
 	delete[] ageLimit;
 	delete[] min_runs;
 
