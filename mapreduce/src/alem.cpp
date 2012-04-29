67b7ff7c7bfdb8781625a2015beb29fa3892f13d
Erik Reed <ereed.uw@gmail.com>
2012-04-29 21:30:31 +0000

    about to remove alem_active bool

--- mapreduce/src/alem.cpp
+++ mapreduce/src/alem.cpp
@@ -10,11 +10,12 @@
 const size_t pop_size = 10; // i.e. EMruns, denoted N
 const size_t numLayers = 6;
 const double agegap = 5; // denoted a
+const bool verbose = true;
 
 FactorGraph fg; // todo: refactor in
 PropertySet infprops;
 
-EMAlg *initEMAlg(FactorGraph fg, PropertySet infprops) {
+EMAlg *initEMAlg(FactorGraph fg, PropertySet &infprops) {
     Evidence* e = new Evidence();
     randomize_fg(&fg);
 //    InfAlgPtr inf = InfAlgPtr(newInfAlg(INF_TYPE, fg, infprops));
@@ -37,6 +38,8 @@ void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t*  min_runs, size_t layer,
 
 	if (emAlgs[layer+1].size() < min_runs[layer+1]) {
 		emAlgs[layer+1].push_back(em);
+		if (verbose)
+			cout << "Moved run from layer " << layer << " to " << layer + 1 << endl;
 	}
 	else {
 		// find out if there is a worse run in next layer
@@ -47,9 +50,11 @@ void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t*  min_runs, size_t layer,
 				EMAlg* discarded = emAlgs[layer+1][j];
 				emAlgs[layer+1].erase(emAlgs[layer+1].begin() + j);
 				delete discarded;
-				cout << "Deleted run from layer " << layer << endl;
+				cout << "Deleted run from layer " << layer +1 << endl;
 				// insert em
 				emAlgs[layer+1].push_back(em);
+				if (verbose)
+					cout << "Moved run from layer " << layer << " to " << layer + 1 << endl;
 				break;
 			}
 		}
@@ -57,6 +62,8 @@ void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t*  min_runs, size_t layer,
 
 	// remove em from original layer
 	emAlgs[layer].erase(emAlgs[layer].begin()+index);
+	if (verbose)
+		cout << "Deleted run from layer " << layer << endl;
 }
 
 void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t* ageLimit) {
@@ -70,20 +77,46 @@ void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t* ageLi
 			if (i == 0 &&  emAlgs[i].size() < min_runs[0]) {
 				// insert k new EM runs
 				int k = min_runs[i] - emAlgs[i].size();
-				for (int j=0; j<k; j++)
-					emAlgs[i].push_back(initEMAlg(fg,infprops));
-				// todo: verify that all of layer 0 is iterated
-				BOOST_FOREACH(EMAlg* em, emAlgs[i]) {
-					em->iterate();
+				for (int j=0; j<k; j++) {
+					EMAlg *newEMalg = initEMAlg(fg, infprops);
+					newEMalg->ALEM_active = true;
+					emAlgs[i].push_back(newEMalg);
+					if (verbose)
+						cout << "Adding run to layer 0. Total in layer 0: "
+						<< emAlgs[i].size() << endl;
 				}
+				// todo: verify that all of layer 0 is iterated
 			}
-			for (size_t j=0; j<emAlgs[i].size(); j++){
+
+			for (int j=emAlgs[i].size()-1; j>=0; j--){
 				EMAlg* em = emAlgs[i][j];
+				assert(em != NULL);
+
+				if (!em->hasSatisfiedTermConditions()) {
+
+					em->iterate();
+					if (verbose)
+						cout << "iteration: layer " << i << ", run " << j <<
+						" iterated to likelihood " << em->logZ() << " and iters=" <<
+						em->Iterations() << endl;
+				}
+
 				// em trial has terminated
-				if (em->hasSatisfiedTermConditions())
+				if (em->ALEM_active && em->hasSatisfiedTermConditions()) {
 					runsTerminated++;
-				else if (em->Iterations() == ageLimit[i])
+					cout << "layer " << i << ", run " << j <<
+							" converged. runsTerminated=" << runsTerminated << endl;
+					// move run to comleted EMs layer emAlgs[numLayers]
+//					EMAlg* converged = emAlgs[i][j];
+//					emAlgs[i].erase(emAlgs[i].begin() + j);
+//					emAlgs[numLayers].push_back(converged);
+				}
+				else if (em->Iterations() >= ageLimit[i]) {
+					if (verbose)
+						cout << "layer " << i << ", run " << j <<
+						" hit age limit of " << ageLimit[i] << endl;
 					checkRuns(emAlgs,min_runs, i, j);
+				}
 			}
 		}
 	}
@@ -93,7 +126,6 @@ void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t* ageLi
 
 int main(int argc, char* argv[]) {
 
-
 	fg.ReadFromFile("dat/fg");
 	infprops.set("verbose", (size_t) 0);
 	infprops.set("updates", string("HUGIN"));
@@ -101,13 +133,13 @@ int main(int argc, char* argv[]) {
 	infprops.set("MAX_ITERS", EM_MAX_ITER);
 
 	vector<vector<EMAlg*> > emAlgs;
-	for (size_t i = 1; i < numLayers; i++)
-		emAlgs.push_back(vector<EMAlg*> ());
+	for (size_t i = 0; i < numLayers; i++)
+		emAlgs.push_back(vector<EMAlg*>());
 
 	// beta_i (age limit of layer i)
 	size_t* ageLimit = new size_t[numLayers];
 	for (size_t i = 0; i < numLayers - 1; i++)
-		ageLimit[i] = agegap * pow(2, i - 1);
+		ageLimit[i] = agegap * pow(2, i);
 	ageLimit[numLayers - 1] = EM_MAX_ITER;
 
 	// M_i (min number of runs of layer i)
@@ -115,17 +147,20 @@ int main(int argc, char* argv[]) {
 	min_runs[0] = 5;
 	for (size_t i = 1; i < numLayers - 1; i++)
 		min_runs[i] = 2;
-	min_runs[numLayers - 1] = EM_MAX_ITER;
+	min_runs[numLayers - 1] = pop_size; // todo: verify
 
 
 	// initialize first layer
-	for (size_t i = 0; i < pop_size; i++) {
+	for (size_t i = 0; i < min_runs[0]; i++) {
 
 		EMAlg *newEMalg = initEMAlg(fg, infprops);
-		newEMalg->iterate();
-
+//		newEMalg->iterate();
+		newEMalg->ALEM_active = true;
 		// add EMAlg to first layer
 		emAlgs[0].push_back(newEMalg);
+		if (verbose)
+			cout << "Adding run to layer 0. Total in layer 0: "
+			<< emAlgs[0].size() << endl;
 	}
 
 	// start ALEM
@@ -133,8 +168,13 @@ int main(int argc, char* argv[]) {
 
 	// collect best likelihood
 	Real bestLikelihood = -1e100;
-	for (size_t i = 0; i < numLayers; i++) {
+	if (verbose)
+		cout << "ALEM complete. Finding best likelihood..." << endl;
+	for (size_t i = 0; i < numLayers; i++) { //  last layer = converged runs
 		for (size_t j = 0; j < emAlgs[i].size(); j++) {
+			if (verbose)
+				cout << "iteration: layer " << i << ", run " << j <<
+				" likelihood: " << emAlgs[i][j]->logZ() << endl;
 			bestLikelihood = max(bestLikelihood, emAlgs[i][j]->logZ());
 			delete emAlgs[i][j];
 		}
