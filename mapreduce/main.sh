551630c083de9d93fdee1a4ce2343bf3a8aa28d5
Erik Reed <ereed.uw@gmail.com>
2012-09-02 22:19:42 +0000

    loop rearrange

--- mapreduce/main.sh
+++ mapreduce/main.sh
@@ -3,16 +3,16 @@
 
 mkdir -p results
 
-trials=1
+trials=1000
 iters=15
 pop=10
 export OMP_NUM_THREADS=4
 
 # asia tests -- 8 nodes, 2hrs
 net=asia
-for samp in 100 200 400 800 1600; do
-	for hid in 2 4 6; do
-		for i in `seq 1 1 $trials`; do
+for i in `seq 1 1 $trials`; do
+	for samp in 100 200 400 800 1600; do
+		for hid in 2 4 6; do
 			echo $net: samples=$samp, hidden=$hid, $i of $trials
 			./scripts/init.sh dat/bnets/$net.net $samp $hid
 			./scripts/test_mapreduce.sh dat/in/ $iters $pop -u > results/$net.$samp.$hid.$i.txt
