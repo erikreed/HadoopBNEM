d79772bc7f5602dcde503648e730bd4296f3b932
Erik Reed <ereed.uw@gmail.com>
2012-09-07 03:57:17 +0000

    running experiments...

--- mapreduce/main.sh
+++ mapreduce/main.sh
@@ -4,8 +4,8 @@
 mkdir -p results
 
 trials=1
-iters=2
-pop=2
+iters=15
+pop=10
 
 # water tests -- 32 nodes
 # single core benchmarks: ~20 seconds per iteration w/ pop 1
@@ -18,8 +18,8 @@ for i in `seq 1 1 $trials`; do
 			echo $net: samples=$samp, hidden=$hid, $i of $trials
 			rm -rf dat/{in,out}
 			./scripts/init.sh dat/bnets/$net.net $samp $hid
-			./scripts/test_mapreduce.sh dat/in/ $iters $pop -u > results/$net.$samp.$hid.$i.txt
-			#./scripts/test_mapreduce.sh dat/in/ $iters $pop -alem > results/$net.$samp.$hid.$i.alem.txt
+			/usr/bin/time -o results/$net.$samp.$hid.$i.log ./scripts/test_mapreduce.sh dat/in/ $iters $pop -u | tee results/$net.$samp.$hid.$i.txt
+			/usr/bin/time -o results/$net.$samp.$hid.$i.alem.log ./scripts/test_mapreduce.sh dat/in/ $iters $pop -alem | tee -a results/$net.$samp.$hid.$i.alem.txt
 		done
 	done
 done
@@ -27,26 +27,3 @@ done
 #rm -rf results
 echo Done!
 exit 0
-
-#################################################
-#################################################
-
-# asia tests -- 8 nodes, 2hrs for ~50 trials
-mkdir -p results
-trials=1000                                                                                  
-iters=15                                                                                     
-pop=10
-
-net=asia
-for i in `seq 1 1 $trials`; do
-	for samp in 100 200 400 800 1600; do
-		for hid in 2 4 6; do
-			echo $net: samples=$samp, hidden=$hid, $i of $trials
-			./scripts/init.sh dat/bnets/$net.net $samp $hid
-			./scripts/test_mapreduce.sh dat/in/ $iters $pop -u > results/$net.$samp.$hid.$i.txt
-			./scripts/test_mapreduce.sh dat/in/ $iters $pop -alem > results/$net.$samp.$hid.$i.alem.txt
-		done
-	done
-done
-7za a $net.results.7z results
-rm -rf results
--- mapreduce/main_hadoop.sh
+++ mapreduce/main_hadoop.sh
@@ -3,12 +3,12 @@
 
 mkdir -p results
 trials=1
-pop=2
-mappers=2
+pop=10
+mappers=16
 
 net=water
 for i in `seq 1 1 $trials`; do
-	for samp in 5; do
+	for samp in 40 80 120 160 200; do
 		for hid in 15; do 	
 			# 15 of 32 nodes hidden
 
@@ -16,11 +16,13 @@ for i in `seq 1 1 $trials`; do
 
 			rm -rf dat/{in,out} in out
 			./scripts/init.sh dat/bnets/$net.net $samp $hid
-			time ./scripts/streaming.sh dat/in -u $mappers $pop 2>&1 | \
+			/usr/bin/time -o results/$net.$samp.$hid.$i.log \
+				./scripts/streaming.sh dat/in -u $mappers $pop 2>&1 | \
 				tee results/$net.$samp.$hid.$i.txt
 			mv dat/out/log.txt results/$net.info
 
-			time ./scripts/streaming.sh dat/in -alem $mappers $pop 2>&1 | \
+			/usr/bin/time -o results/$net.$samp.$hid.$i.alem.log \
+				./scripts/streaming.sh dat/in -alem $mappers $pop 2>&1 | \
 				tee results/$net.$samp.$hid.$i.alem.txt
 			mv dat/out/log.txt results/$net.alem.info
 		done
