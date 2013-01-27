29e2816dcdcd99c68b98dd5d64e09de9104c77c3
Erik Reed <ereed.uw@gmail.com>
2013-01-27 20:43:01 +0000

    switch to google style guide

--- mapreduce/src/alem.cpp
+++ mapreduce/src/alem.cpp
@@ -15,194 +15,194 @@ const char* TAB_PATH = "dat/in/tab";
 const char* FG_PATH = "dat/in/fg";
 
 vector<EMAlg*> getAllRuns(vector<vector<EMAlg*> > &emAlgs) {
-	vector<EMAlg*> ems;
-	foreach(vector<EMAlg*> &layer, emAlgs) {
-		foreach(EMAlg* em, layer) {
-			ems.push_back(em);
-		}
-	}
-	return ems;
+  vector<EMAlg*> ems;
+  foreach(vector<EMAlg*> &layer, emAlgs) {
+    foreach(EMAlg* em, layer) {
+      ems.push_back(em);
+    }
+  }
+  return ems;
 }
 
 // tons of memory leaks in here
 EMAlg *initEMAlg(FactorGraph fg, PropertySet &infprops) {
-	Evidence* e = new Evidence();
-	randomize_fg(&fg);
-	//    InfAlgPtr inf = InfAlgPtr(newInfAlg(INF_TYPE, fg, infprops));
-	//    inf.get()->init();
-	InfAlg* inf = newInfAlg(INF_TYPE, *fg.clone(), infprops);
+  Evidence* e = new Evidence();
+  randomize_fg(&fg);
+  //    InfAlgPtr inf = InfAlgPtr(newInfAlg(INF_TYPE, fg, infprops));
+  //    inf.get()->init();
+  InfAlg* inf = newInfAlg(INF_TYPE, *fg.clone(), infprops);
 
-	ifstream estream(TAB_PATH);
-	e->addEvidenceTabFile(estream, *fg.clone());
-	// Read EM specification
-	ifstream emstream(EM_PATH);
-	EMAlg *newEMalg = new EMAlg(*e, *inf, emstream);
-	return newEMalg;
+  ifstream estream(TAB_PATH);
+  e->addEvidenceTabFile(estream, *fg.clone());
+  // Read EM specification
+  ifstream emstream(EM_PATH);
+  EMAlg *newEMalg = new EMAlg(*e, *inf, emstream);
+  return newEMalg;
 }
 
-void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t*  min_runs, size_t layer, size_t index) {
-	assert(layer+1 < emAlgs.size());
+void checkRuns(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t layer,
+               size_t index) {
+  assert(layer + 1 < emAlgs.size());
 
-	EMAlg* em = emAlgs[layer][index];
-	Real likelihood = em->logZ();
+  EMAlg* em = emAlgs[layer][index];
+  Real likelihood = em->logZ();
 
-	if (emAlgs[layer+1].size() < min_runs[layer+1]) {
-		emAlgs[layer+1].push_back(em);
-		if (verbose)
-			cout << "Moved run from layer " << layer << " to " << layer + 1 << endl;
-	} else {
-		// find out if there is a worse run in next layer
-		for (size_t j=0; j<emAlgs[layer+1].size(); j++) {
-			Real next = emAlgs[layer+1][j]->logZ();
-			if (next < likelihood) {
-				// discard em_next
-				EMAlg* discarded = emAlgs[layer+1][j];
-				emAlgs[layer+1].erase(emAlgs[layer+1].begin() + j);
-				delete discarded;
-				if (verbose)
-					cout << "Discarded run from layer " << layer +1 << endl;
-				// insert em
-				emAlgs[layer+1].push_back(em);
-				if (verbose)
-					cout << "Moved (better) run from layer " << layer << " to " << layer + 1 << endl;
-				break;
-			}
-		}
-	}
+  if (emAlgs[layer + 1].size() < min_runs[layer + 1]) {
+    emAlgs[layer + 1].push_back(em);
+    if (verbose)
+      cout << "Moved run from layer " << layer << " to " << layer + 1 << endl;
+  } else {
+    // find out if there is a worse run in next layer
+    for (size_t j = 0; j < emAlgs[layer + 1].size(); j++) {
+      Real next = emAlgs[layer + 1][j]->logZ();
+      if (next < likelihood) {
+        // discard em_next
+        EMAlg* discarded = emAlgs[layer + 1][j];
+        emAlgs[layer + 1].erase(emAlgs[layer + 1].begin() + j);
+        delete discarded;
+        if (verbose)
+          cout << "Discarded run from layer " << layer + 1 << endl;
+        // insert em
+        emAlgs[layer + 1].push_back(em);
+        if (verbose)
+          cout << "Moved (better) run from layer " << layer << " to " << layer
+              + 1 << endl;
+        break;
+      }
+    }
+  }
 
-	// remove em from original layer
-	emAlgs[layer].erase(emAlgs[layer].begin()+index);
-	if (verbose)
-		cout << "Deleted run from layer " << layer << endl;
+  // remove em from original layer
+  emAlgs[layer].erase(emAlgs[layer].begin() + index);
+  if (verbose)
+    cout << "Deleted run from layer " << layer << endl;
 }
 
-void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs, size_t* ageLimit) {
+void ALEM_check(vector<vector<EMAlg*> > &emAlgs, size_t* min_runs,
+                size_t* ageLimit) {
 
-	size_t runsTerminated = 0;
-	// while non-terminated runs exist
-	while (runsTerminated < pop_size) {
+  size_t runsTerminated = 0;
+  // while non-terminated runs exist
+  while (runsTerminated < pop_size) {
 
-		// add EMs to first layer
-		if (emAlgs[0].size() < min_runs[0]) {
-			// insert k new EM runs
-			//			int k = min_runs[0] - emAlgs[0].size();
-			int k = min_runs[0] - getNumRuns(emAlgs);
-			for (int j=0; j<k; j++) {
-				emAlgs[0].push_back(initEMAlg(fg,infprops));
-				if (verbose)
-					cout << "Adding run to layer 0. Total in layer 0: "
-					<< emAlgs[0].size() << endl;
-			}
-		}
+    // add EMs to first layer
+    if (emAlgs[0].size() < min_runs[0]) {
+      // insert k new EM runs
+      //			int k = min_runs[0] - emAlgs[0].size();
+      int k = min_runs[0] - getNumRuns(emAlgs);
+      for (int j = 0; j < k; j++) {
+        emAlgs[0].push_back(initEMAlg(fg, infprops));
+        if (verbose)
+          cout << "Adding run to layer 0. Total in layer 0: "
+              << emAlgs[0].size() << endl;
+      }
+    }
 
-		// iterate EMs over all layers
-		vector<EMAlg*> ems = getAllRuns(emAlgs);
+    // iterate EMs over all layers
+    vector<EMAlg*> ems = getAllRuns(emAlgs);
 #pragma omp parallel for
-		for (size_t i=0; i<ems.size(); i++) {
-			EMAlg* em = ems[i];
-			if (!em->hasSatisfiedTermConditions()) {
-				em->ALEM_active = true;
-				em->iterate();
-				if (verbose)
-					cout << "iteration: layer " << i <<
-					" iterated to likelihood " << em->logZ() << " and iters=" <<
-					em->Iterations() << endl;
-			}
-		}
+    for (size_t i = 0; i < ems.size(); i++) {
+      EMAlg* em = ems[i];
+      if (!em->hasSatisfiedTermConditions()) {
+        em->ALEM_active = true;
+        em->iterate();
+        if (verbose)
+          cout << "iteration: layer " << i << " iterated to likelihood "
+              << em->logZ() << " and iters=" << em->Iterations() << endl;
+      }
+    }
 
-		// perform ALEM likelihood checking and move EMs
-		// between layers
-		for (size_t i=0; i<numLayers; i++) {
-			for (int j=emAlgs[i].size()-1; j>=0; j--){
-				EMAlg* em = emAlgs[i][j];
-				// em trial has terminated
-				if (em->hasSatisfiedTermConditions() && em->ALEM_active) {
-					em->ALEM_active = false;
-					runsTerminated++;
-					if (verbose) {
-						cout << "layer " << i << ", run " << j <<
-						" converged. runsTerminated=" << runsTerminated << endl;
-					}
-					// move run to completed EMs layer emAlgs[numLayers-1]
-					EMAlg* converged = emAlgs[i][j];
-					emAlgs[i].erase(emAlgs[i].begin() + j);
-					emAlgs[numLayers-1].push_back(converged);
-				} else if (i < numLayers && em->Iterations() >= ageLimit[i]) {
-					if (verbose)
-						cout << "layer " << i << ", run " << j <<
-						" hit age limit of " << ageLimit[i] << endl;
-					checkRuns(emAlgs,min_runs, i, j);
-				}
-			}
-		}
-	}
+    // perform ALEM likelihood checking and move EMs
+    // between layers
+    for (size_t i = 0; i < numLayers; i++) {
+      for (int j = emAlgs[i].size() - 1; j >= 0; j--) {
+        EMAlg* em = emAlgs[i][j];
+        // em trial has terminated
+        if (em->hasSatisfiedTermConditions() && em->ALEM_active) {
+          em->ALEM_active = false;
+          runsTerminated++;
+          if (verbose) {
+            cout << "layer " << i << ", run " << j
+                << " converged. runsTerminated=" << runsTerminated << endl;
+          }
+          // move run to completed EMs layer emAlgs[numLayers-1]
+          EMAlg* converged = emAlgs[i][j];
+          emAlgs[i].erase(emAlgs[i].begin() + j);
+          emAlgs[numLayers - 1].push_back(converged);
+        } else if (i < numLayers && em->Iterations() >= ageLimit[i]) {
+          if (verbose)
+            cout << "layer " << i << ", run " << j << " hit age limit of "
+                << ageLimit[i] << endl;
+          checkRuns(emAlgs, min_runs, i, j);
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
-	rnd_seed(time(NULL));
-	fg.ReadFromFile(FG_PATH);
-	infprops = getProps();
+  rnd_seed(time(NULL));
+  fg.ReadFromFile(FG_PATH);
+  infprops = getProps();
 
-	vector<vector<EMAlg*> > emAlgs;
-	for (size_t i = 0; i < numLayers ; i++)
-		emAlgs.push_back(vector<EMAlg*> ());
+  vector<vector<EMAlg*> > emAlgs;
+  for (size_t i = 0; i < numLayers; i++)
+    emAlgs.push_back(vector<EMAlg*> ());
 
-	// beta_i (age limit of layer i)
-	size_t* ageLimit = new size_t[numLayers];
-	for (size_t i = 0; i < numLayers - 1; i++)
-		ageLimit[i] = agegap * pow(2, i);
-	ageLimit[numLayers - 1] = EM_MAX_ITER;
+  // beta_i (age limit of layer i)
+  size_t* ageLimit = new size_t[numLayers];
+  for (size_t i = 0; i < numLayers - 1; i++)
+    ageLimit[i] = agegap * pow(2, i);
+  ageLimit[numLayers - 1] = EM_MAX_ITER;
 
-	// M_i (min number of runs of layer i)
-	size_t* min_runs = new size_t[numLayers];
-	min_runs[0] = min_runs_layer0;
-	for (size_t i = 1; i < numLayers - 1; i++) {
-		min_runs[i] = min_runs_intermediate;
-	}
-	min_runs[numLayers - 1] = pop_size;
+  // M_i (min number of runs of layer i)
+  size_t* min_runs = new size_t[numLayers];
+  min_runs[0] = min_runs_layer0;
+  for (size_t i = 1; i < numLayers - 1; i++) {
+    min_runs[i] = min_runs_intermediate;
+  }
+  min_runs[numLayers - 1] = pop_size;
 
+  // initialize first layer
+  for (size_t i = 0; i < min_runs[0]; i++) {
 
-	// initialize first layer
-	for (size_t i = 0; i < min_runs[0]; i++) {
+    EMAlg *newEMalg = initEMAlg(fg, infprops);
+    //		newEMalg->iterate();
 
-		EMAlg *newEMalg = initEMAlg(fg, infprops);
-		//		newEMalg->iterate();
+    // add EMAlg to first layer
+    emAlgs[0].push_back(newEMalg);
+    if (verbose)
+      cout << "Adding run to layer 0. Total in layer 0: " << emAlgs[0].size()
+          << endl;
+  }
 
-		// add EMAlg to first layer
-		emAlgs[0].push_back(newEMalg);
-		if (verbose)
-			cout << "Adding run to layer 0. Total in layer 0: "
-			<< emAlgs[0].size() << endl;
-	}
+  // start ALEM
+  ALEM_check(emAlgs, min_runs, ageLimit);
 
-	// start ALEM
-	ALEM_check(emAlgs, min_runs, ageLimit);
+  // collect best likelihood
+  Real bestLikelihood = -1e100;
+  size_t bestIters = -1;
+  cout << "ALEM complete. Finding best likelihood..." << endl;
+  for (size_t i = 0; i < numLayers; i++) { //  last layer = converged runs
+    for (size_t j = 0; j < emAlgs[i].size(); j++) {
+      EMAlg* em = emAlgs[i][j];
+      // only print out the inactive/converged EMs
+      if (em->hasSatisfiedTermConditions())
+        cout << "converged: layer " << i << ", run " << j << " likelihood: "
+            << em->logZ() << " iters: " << em->Iterations() << endl;
+      if (em->logZ() > bestLikelihood) {
+        bestLikelihood = em->logZ();
+        bestIters = em->Iterations();
+      }
 
-	// collect best likelihood
-	Real bestLikelihood = -1e100;
-	size_t bestIters = -1;
-	cout << "ALEM complete. Finding best likelihood..." << endl;
-	for (size_t i = 0; i < numLayers; i++) { //  last layer = converged runs
-		for (size_t j = 0; j < emAlgs[i].size(); j++) {
-			EMAlg* em = emAlgs[i][j];
-			// only print out the inactive/converged EMs
-			if (em->hasSatisfiedTermConditions())
-				cout << "converged: layer " << i << ", run " << j <<
-				" likelihood: " << em->logZ() << " iters: " <<
-				em->Iterations() << endl;
-			if (em->logZ() > bestLikelihood) {
-				bestLikelihood = em->logZ();
-				bestIters = em->Iterations();
-			}
+      delete em;
+    }
+  }
 
-			delete em;
-		}
-	}
+  cout << "Best: " << bestLikelihood << " iters: " << bestIters << endl;
 
-	cout << "Best: " << bestLikelihood << " iters: " << bestIters << endl;
+  delete[] ageLimit;
+  delete[] min_runs;
 
-	delete[] ageLimit;
-	delete[] min_runs;
-
-	return 0;
+  return 0;
 }
--- mapreduce/src/dai_reduce.cpp
+++ mapreduce/src/dai_reduce.cpp
@@ -5,129 +5,129 @@
 #include <map>
 
 Real EM_estep(MaximizationStep &mstep, const Evidence &evidence, InfAlg &inf) {
-   Real logZ = 0;
-   Real likelihood = 0;
+  Real logZ = 0;
+  Real likelihood = 0;
 
-   inf.run();
-   logZ = inf.logZ();
+  inf.run();
+  logZ = inf.logZ();
 
-   // Expectation calculation
-   for (Evidence::const_iterator e = evidence.begin(); e != evidence.end(); ++e) {
-      InfAlg* clamped = inf.clone();
-      // Apply evidence
-      for (Evidence::Observation::const_iterator i = e->begin(); i != e->end(); ++i)
-         clamped->clamp(clamped->fg().findVar(i->first), i->second);
-      clamped->init();
-      clamped->run();
+  // Expectation calculation
+  for (Evidence::const_iterator e = evidence.begin(); e != evidence.end(); ++e) {
+    InfAlg* clamped = inf.clone();
+    // Apply evidence
+    for (Evidence::Observation::const_iterator i = e->begin(); i != e->end(); ++i)
+      clamped->clamp(clamped->fg().findVar(i->first), i->second);
+    clamped->init();
+    clamped->run();
 
-      likelihood += clamped->logZ() - logZ;
+    likelihood += clamped->logZ() - logZ;
 
-      mstep.addExpectations(*clamped);
+    mstep.addExpectations(*clamped);
 
-      delete clamped;
-   }
+    delete clamped;
+  }
 
-   return likelihood;
+  return likelihood;
 }
 
 bool emHasSatisfiedTermConditions(size_t iter, Real previous, Real current) {
-   if (iter >= EM_MAX_ITER)
-      return true;
-   else if (iter < 3)
-      // need at least 2 to calculate ratio
-      // Also, throw away first iteration, as the parameters may not
-      // have been normalized according to the estimation method
+  if (iter >= EM_MAX_ITER)
+    return true;
+  else if (iter < 3)
+    // need at least 2 to calculate ratio
+    // Also, throw away first iteration, as the parameters may not
+    // have been normalized according to the estimation method
+    return false;
+  else {
+    if (previous == 0)
       return false;
-   else {
-      if (previous == 0)
-         return false;
-      Real diff = current - previous;
-      if (diff < 0) {
-         std::cerr << "Error: in EM log-likehood decreased from " << previous << " to " << current
-               << std::endl;
-         return true;
-      }
-      return (diff / fabs(previous)) <= LIB_EM_TOLERANCE;
-   }
+    Real diff = current - previous;
+    if (diff < 0) {
+      std::cerr << "Error: in EM log-likehood decreased from " << previous
+          << " to " << current << std::endl;
+      return true;
+    }
+    return (diff / fabs(previous)) <= LIB_EM_TOLERANCE;
+  }
 }
 
 EMdata em_reduce(vector<EMdata>& in) {
-   // using first EMdata to store e-step counts and create fg
-   EMdata dat = in[0];
-   if (dat.isConverged())
-      return dat;
-   FactorGraph fg = dat.fg;
+  // using first EMdata to store e-step counts and create fg
+  EMdata dat = in[0];
+  if (dat.isConverged())
+    return dat;
+  FactorGraph fg = dat.fg;
 
-   // collect stats for each set of evidence
-   for (size_t i = 1; i < in.size(); i++) {
-      EMdata next = in[i];
-      DAI_ASSERT(dat.msteps.size() == next.msteps.size());
-      DAI_ASSERT(dat.bnID == next.bnID && dat.ALEM_layer == next.ALEM_layer);
+  // collect stats for each set of evidence
+  for (size_t i = 1; i < in.size(); i++) {
+    EMdata next = in[i];
+    DAI_ASSERT(dat.msteps.size() == next.msteps.size());
+    DAI_ASSERT(dat.bnID == next.bnID && dat.ALEM_layer == next.ALEM_layer);
 
-      for (size_t j = 0; j < dat.msteps.size(); j++)
-         dat.msteps[j].addExpectations(next.msteps[j]);
+    for (size_t j = 0; j < dat.msteps.size(); j++)
+      dat.msteps[j].addExpectations(next.msteps[j]);
 
-      dat.likelihood += next.likelihood;
-   }
+    dat.likelihood += next.likelihood;
+  }
 
-   // m-step
-   for (size_t i = 0; i < dat.msteps.size(); i++)
-      dat.msteps[i].maximize(fg);
+  // m-step
+  for (size_t i = 0; i < dat.msteps.size(); i++)
+    dat.msteps[i].maximize(fg);
 
-   // save new fg, clear counts, increment iter
-   dat.fg = fg;
-   dat.msteps.clear();
-   dat.iter++;
+  // save new fg, clear counts, increment iter
+  dat.fg = fg;
+  dat.msteps.clear();
+  dat.iter++;
 
-   return dat;
+  return dat;
 }
 
 int main(int argc, char* argv[]) {
-   //read data from mappers
-   ostringstream ss;
+  //read data from mappers
+  ostringstream ss;
 
-   string s;
-   while (std::getline(std::cin, s))
-      ss << s << '\n';
+  string s;
+  while (std::getline(std::cin, s))
+    ss << s << '\n';
 
-   string input = ss.str();
+  string input = ss.str();
 
-   vector<string> data = str_split(input, '\n');
-   assert(data.size() > 0);
+  vector<string> data = str_split(input, '\n');
+  assert(data.size() > 0);
 
-   map<int, vector<EMdata> > idToDat;
+  map<int, vector<EMdata> > idToDat;
 
-   for (size_t i = 0; i < data.size(); i++) {
-      string line = data[i];
-      vector<string> parts = str_split(line, '*');
-      assert(parts.size() == 2);
-      try {
-         EMdata dat = stringToEM(parts[1]); // value
-         int id = atoi(parts[0].c_str()); // key
-         assert(id == dat.bnID && id >= 0);
-         if (idToDat.count(id) == 0) {
-            idToDat[id] = vector<EMdata> ();
-         }
-         idToDat[id].push_back(dat);
-      } catch(int e) {
-         cerr << "Exception " << e << " on loop " << i << endl;
-         cerr << "Key: " << parts[0] << endl;
-         cerr << "Data passed to stringToEM:" << endl;
-         cerr << parts[1] << endl;
-         cerr << endl << endl << "Full input:" << endl;
-         cerr << input << endl;
-         throw 7;
+  for (size_t i = 0; i < data.size(); i++) {
+    string line = data[i];
+    vector<string> parts = str_split(line, '*');
+    assert(parts.size() == 2);
+    try {
+      EMdata dat = stringToEM(parts[1]); // value
+      int id = atoi(parts[0].c_str()); // key
+      assert(id == dat.bnID && id >= 0);
+      if (idToDat.count(id) == 0) {
+        idToDat[id] = vector<EMdata> ();
       }
-   }
+      idToDat[id].push_back(dat);
+    } catch (int e) {
+      cerr << "Exception " << e << " on loop " << i << endl;
+      cerr << "Key: " << parts[0] << endl;
+      cerr << "Data passed to stringToEM:" << endl;
+      cerr << parts[1] << endl;
+      cerr << endl << endl << "Full input:" << endl;
+      cerr << input << endl;
+      throw 7;
+    }
+  }
 
-   for (map<int, std::vector<EMdata> >::iterator iter = idToDat.begin();
-         iter != idToDat.end(); iter++) {
-      EMdata out = em_reduce(iter->second);
-      out.emFile = ""; // reduce amount of serialization
-      out.tabFile = "";
-      string outstring = emToString(out);
-      cout << outstring << endl;
-   }
+  for (map<int, std::vector<EMdata> >::iterator iter = idToDat.begin(); iter
+      != idToDat.end(); iter++) {
+    EMdata out = em_reduce(iter->second);
+    out.emFile = ""; // reduce amount of serialization
+    out.tabFile = "";
+    string outstring = emToString(out);
+    cout << outstring << endl;
+  }
 
-   return 0;
+  return 0;
 }
