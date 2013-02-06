db5575c821d3b2259133ff4afaefc42e673bceda
Erik Reed <ereed.uw@gmail.com>
2013-02-06 04:24:18 +0000

    large population tst

---
+++ mapreduce/tests/test-large-population.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+# erik reed
+
+pop=10000
+mappers=2
+reducers=2
+net=alarm
+
+SAMPLES=1
+HIDDEN=rand
+
+rm -rf dat/{in,out} in out
+./scripts/init.sh dat/bnets/$net.net $SAMPLES $HIDDEN
+echo $net: pop=$pop, mappers=$mappers
+
+/usr/bin/time ./scripts/streaming.sh dat/in -u $mappers $reducers $pop
+rm -rf dat/{in,out} in out
+
