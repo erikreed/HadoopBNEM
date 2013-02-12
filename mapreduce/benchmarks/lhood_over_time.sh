405940d942290a7b179321d45c9f5329b28f4bcd
Erik Reed <ereed.uw@gmail.com>
2013-02-12 22:56:58 +0000

    lhood benchmarks

---
+++ mapreduce/benchmarks/lhood_over_time.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+# erik reed
+
+net=asia
+pop=1
+
+trials=1000
+SAMPLES=1000
+HIDDEN=rand
+max_iters=100000
+dir=results_lhood
+
+
+export time_duration=5
+
+mkdir -p $dir
+
+rm -rf dat/{in,out} in out
+./scripts/init.sh dat/bnets/$net.net $SAMPLES $HIDDEN
+echo $net: pop=$pop, max_iters=$max_iters
+
+
+for trial in `seq 1 1 $trials`; do
+  name=$dir/$net.$pop.$trial
+  rm -f $name
+  export start_time=`date +%s`
+  while true; do
+    /usr/bin/time ./scripts/test_mapreduce.sh dat/in $max_iters $pop -u | \
+        tee -a $name
+    
+    if [ -n "$start_time" -a -n "$time_duration" ]; then
+      dur=$((`date +%s` - $start_time))
+      if [ $dur -ge $time_duration ]; then
+        echo $time_duration seconds reached! Quitting...
+        break
+      fi
+    fi
+  done
+done
+
